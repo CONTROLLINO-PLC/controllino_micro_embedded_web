ecde04bfa0454605d0a7c38b1abe2bde9a5d7249
Pedro Marquez <pedro@controllino.com>
2023-11-02 12:06:49 +0100

    build script also for linux and mac

--- build_web.py
+++ build_web.py
@@ -1,33 +1,53 @@
+import sys
 import os
 import subprocess
 import shutil
 
 def main():
+    _win32 = False
+    if sys.platform == "win32":
+        _win32 = True
+
     # Remove web build directory
-    if (os.path.exists("./lib/webapp/web")):
-        shutil.rmtree("./lib/webapp/web")
+    if (os.path.exists("lib/webapp/web")):
+        shutil.rmtree("lib/webapp/web")
 
     # Build web app
-    os.chdir("./webdev")
-    subprocess.run(["npm", "install"], shell=True)
-    subprocess.run(["npm", "run", "build"], shell=True)
+    os.chdir("webdev")
+    if _win32:
+        subprocess.run(["npm", "install"], shell=True)
+        subprocess.run(["npm", "run", "build"], shell=True)
+    else:	
+        subprocess.run(["npm install"], shell=True)
+        subprocess.run(["npm run build"], shell=True)
     os.chdir("..")
 
     # Compile pack.c
-    subprocess.run(["gcc", "./tools/pack.c"], shell=True)
+    if _win32:
+        subprocess.run(["gcc", "tools/pack.c"], shell=True)
+    else:
+        subprocess.run(["gcc tools/pack.c"], shell=True)
 
     # Pack web files into packed_fs.c
-    os.chdir("./lib/webapp")
+    os.chdir("lib/webapp")
     packed_fs_file = open("packed_fs.c", "w")
-    subprocess.run(
+    if _win32:
+        subprocess.run(
+            [
+                "..\\..\\a.exe",
+                "web/*.gz", 
+                "web/*.ico",
+                "web/assets/*.gz", 
+                "web/assets/*.png"
+            ], 
+            shell=True , stdout=packed_fs_file)
+    else:
+        subprocess.run(
         [
-            "..\\..\\a",
-            "web/*.gz", 
-            "web/*.ico",
-            "web/assets/*.gz", 
-            "web/assets/*.png"
+            "../../a.out web/*.gz web/*.ico web/assets/*.gz web/assets/*.png"
         ], 
         shell=True , stdout=packed_fs_file)
     packed_fs_file.close()
+    print(sys.platform)
 
 main()
