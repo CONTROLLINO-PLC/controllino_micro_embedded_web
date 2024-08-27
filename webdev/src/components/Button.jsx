9d217b5d06751e6013f19c885e1193a530126522
busyalltime <luffytaro94@gmx.com>
2024-08-27 19:45:06 -0400

    fixing componnents

--- webdev/src/components/Button.jsx
+++ webdev/src/components/Button.jsx
@@ -1,4 +1,12 @@
+import PropTypes from 'prop-types';
+
 export function Button({ className, onClick, bgClassName, ...props }) {
+  // Add className to props validation
+  Button.propTypes = {
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    bgClassName: PropTypes.string,
+  };
   return (
     <button
       className={`py-1 px-auto disabled:opacity-25 disabled:cursor-not-allowed rounded-full font-bold ${bgClassName ? bgClassName : 'bg-primary hover:bg-primary/70 text-black'} transition-transform duration-500 ease-in-out transform active:scale-95 ` + className}
