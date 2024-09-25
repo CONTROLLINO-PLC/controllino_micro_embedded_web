79c5df8e0c9a88211c0bc37e9cfab96837083dbd
busyalltime <luffytaro94@gmx.com>
2024-09-25 14:18:29 -0400

    the slider only sends the value when it is the final one

--- webdev/src/components/Slider.jsx
+++ webdev/src/components/Slider.jsx
@@ -1,21 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Slider({ value, onChange }) {
     const [selected, setSelected] = useState(false);
+    const [localValue, setLocalValue] = useState(value);
+
+    useEffect(() => {
+        setLocalValue(value);
+    }, [value]);
+
+    const handleSliderChange = (e) => {
+        setLocalValue(e.target.value);
+    };
+
+    const handleSliderChangeEnd = () => {
+        setSelected(false);
+        onChange({ target: { value: localValue } });
+    };
 
     return (
         <div className="relative w-full flex items-center">
             <div className="absolute inset-x-0 h-1 bg-primary/40 rounded-full" />
             <div
                 id="sliderTrack"
-                className={`absolute h-1.5 rounded-full bg-primary ${value > 0 ? 'w-full' : 'w-0'}`}
-                style={{ width: `${value}%` }}
+                className={`absolute h-1.5 rounded-full bg-primary ${localValue > 0 ? 'w-full' : 'w-0'}`}
+                style={{ width: `${localValue}%` }}
             />
 
             <div
                 id="sliderThumb"
                 className={"absolute my-auto -m-2.5 w-5 h-5 rounded-full bg-primary"}
-                style={{ left: `${value}%` }}
+                style={{ left: `${localValue}%` }}
             >
                 {selected && <div className="w-full h-full bg-primary rounded-full animate-ping" />}
             </div>
@@ -26,9 +40,10 @@ export function Slider({ value, onChange }) {
                 min="0"
                 max="100"
                 onMouseDown={() => setSelected(true)}
-                onMouseUp={() => setSelected(false)}
-                value={value}
-                onChange={onChange}
+                onMouseUp={handleSliderChangeEnd}
+                onTouchEnd={handleSliderChangeEnd}
+                value={localValue}
+                onChange={handleSliderChange}
                 className="absolute w-full h-1 opacity-0 cursor-pointer"
                 style={{ top: '50%', transform: 'translateY(-50%)' }}
             />
